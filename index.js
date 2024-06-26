a2b19d85eda8f6a350f00524165d45f9ba5fb847
7S-Coder <dylan.e33@hotmail.fr>
2024-06-27 01:39:05 +0200

    optimised functions

--- index.js
+++ index.js
@@ -4,19 +4,51 @@ function updateNumber(updateFunc) {
   let number = parseInt(numberElement.textContent, 10);
   number = updateFunc(number);
   numberElement.textContent = number;
-}
-
-// Fonction pour incrémenter le nombre de 1
-function addOneAtJouons() {
-  updateNumber((number) => number + 1);
-}
-
-// Fonction pour incrémenter le nombre de 10
-function addTenAtJouons() {
-  updateNumber((number) => number + 10);
+  return number; // Retourner le nombre mis à jour
 }
 
 // Fonction pour réinitialiser le nombre à 0
 function resetJouons() {
-  updateNumber(() => 0);
+  let numberElement = document.getElementById("number");
+  numberElement.textContent = 0;
+}
+
+// Vérifier si le nombre dépasse une limite et le réinitialiser
+function checkAndReset(number) {
+  let limit = 25;
+  if (number >= limit || number <= -limit) {
+    resetJouons();
+  }
+}
+
+// Fonction pour incrémenter le nombre de 1
+function addOneAtJouons() {
+  let updatedNumber = updateNumber((number) => {
+    return number + 1;
+  });
+  checkAndReset(updatedNumber);
+}
+
+// Fonction pour incrémenter le nombre de 1
+function deleteOneAtJouons() {
+  let updatedNumber = updateNumber((number) => {
+    return number - 1;
+  });
+  checkAndReset(updatedNumber);
+}
+
+// Fonction pour incrémenter le nombre de 10
+function addTenAtJouons() {
+  let updatedNumber = updateNumber((number) => {
+    return number + 10;
+  });
+  checkAndReset(updatedNumber);
+}
+
+// Fonction pour décrémenter le nombre de 10
+function deleteTenAtJouons() {
+  let updatedNumber = updateNumber((number) => {
+    return number - 10;
+  });
+  checkAndReset(updatedNumber);
 }
