bddeb875d6b831cbd76aabca47b016dcfd5eef61
7S-Coder <dylan.e33@hotmail.fr>
2024-06-27 00:53:30 +0200

    update name of the fonctions to  addOne

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-function addOne() {
+function addOneAtJouons() {
   let numberElement = document.getElementById("number");
   let number = parseInt(numberElement.textContent, 10);
   number++;
   numberElement.textContent = number;
-  console.log(number);
 }
