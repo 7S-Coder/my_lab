b9c4e72ce4e159ad6d69cf7d3e527d27e4b55e65
7S-Coder <dylan.e33@hotmail.fr>
2024-06-27 01:22:40 +0200

    optimised my functions

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-function addOneAtJouons() {
+// Fonction qui met à jour le nombre
+function updateNumber(updateFunc) {
   let numberElement = document.getElementById("number");
   let number = parseInt(numberElement.textContent, 10);
-  number++;
+  number = updateFunc(number);
   numberElement.textContent = number;
+}
+
+// Fonction pour incrémenter le nombre de 1
+function addOneAtJouons() {
+  updateNumber((number) => number + 1);
+}
+
+// Fonction pour incrémenter le nombre de 10
+function addTenAtJouons() {
+  updateNumber((number) => number + 10);
+}
+
+// Fonction pour réinitialiser le nombre à 0
+function resetJouons() {
+  updateNumber(() => 0);
 }
